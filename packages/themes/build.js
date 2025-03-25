9d0b36c95874ec37abd97f38f6fe248df5751639
권욱헌 <ukheon.keon@happyblock.io>
2025-03-25 16:26:13 +0900

    Feat: 프로젝트 구조 및 설정 파일 추가
    - .yarnrc.yml 파일 생성하여 Yarn 버전 설정
    - biome.jsonc 파일 추가하여 Biome 스키마 및 코드 포맷팅 규칙 정의
    - nx.json 파일 추가하여 Nx 작업 공간 설정
    - tsconfig.base.json 파일 생성하여 TypeScript 컴파일러 옵션 설정
    - package.json 파일 수정하여 프로젝트 이름 변경 및 devDependencies 추가
    - esbuild-config 패키지 추가 및 빌드 스크립트 설정
    - themes 패키지의 build.js 파일 수정하여 esbuild-config 사용
    - 테스트 하기 위해 사용한 service/test 제거

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,31 +1,6 @@
-import esbuild from "esbuild";
+import run from "@ukheon/esbuild-config";
+import pkg from "./package.json" assert { type: "json" };
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  target: "es2019",
-  watch
-};
-
-esbuild.build({
-  ...baseConfig,
-  format: "esm",
-});
-
-esbuild.build({
-  ...baseConfig,
-  format: "cjs",
-  outExtension: {
-    ".js": ".cjs",
-  },
-}).catch((err) => {
-  console.error(...err);
-});
+run({
+  pkg,
+})
